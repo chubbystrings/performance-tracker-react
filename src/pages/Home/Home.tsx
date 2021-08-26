1ae706ed217e4dcb9ba17801f61c692bd1ca24a9
chubbystrings <48825855+chubbystrings@users.noreply.github.com>
2021-08-26 12:03:59 +0100

    homepage done

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import classes from "./Home.module.css";
+import decagonLogo from "../../assets/img/logo.png";
+import teamsLogo from "../../assets/img/Teams-logo.png";
 
 const Home: React.FC = () => {
-    return (
-        <div>This is Home pae</div>
-    )
-}
+  return (
+    <main className={classes["login__page"]}>
+      <section className={classes["sidebar__section"]}></section>
+      <section className={classes["main__section"]}>
+        <div>
+          <div className={classes["tag__wrapper"]}>
+            <img
+              src={decagonLogo}
+              className={classes["img__res"]}
+              alt="decagon logo"
+            />
+          </div>
+          <div className={classes["tag__wrapper"]}>
+            <h4>Welcome to Decagon's Learning Tracker</h4>
+          </div>
+          <div className={classes["tag__wrapper"]}>
+            <img
+              src={teamsLogo}
+              className={classes["img__res"]}
+              alt="teams logo"
+            />
+          </div>
+          <div className={classes["tag__wrapper"]}>
+            <Link className={classes["btn__large"]} to="/dashboard">Login With Microsoft Teams</Link>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
 
-export default Home
+export default Home;
